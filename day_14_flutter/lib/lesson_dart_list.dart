2edb861a45614fb188440e7ade4e8a6341c765e5
Blizzard-10 <blizzard-10@Admin-10s-iMac.local>
2023-07-26 12:40:51 +0800

    add: day_14_flutter

---
+++ day_14_flutter/lib/lesson_dart_list.dart
@@ -0,0 +1,11 @@
+
+
+var huvsgulZam = ['car1', 'car2', 'car3', 'car4', 5];
+
+List<String> suuniiUildwer = ['suu1', 'suu2', 'suu3', '5'];
+void main(){
+  print(huvsgulZam);
+  print(huvsgulZam[0]);
+  print(huvsgulZam[3]);
+  print(huvsgulZam[4]);
+}
