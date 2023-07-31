3d5e3272daefb243887422a3dbca6ff509326720
Blizzard-10 <blizzard-10@Admin-10s-iMac.local>
2023-07-31 13:00:43 +0800

    word_find_6

---
+++ wordfind_app/lib/Start_Button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class StartButton extends StatelessWidget {
+  final String text;
+  const StartButton(this.text,{super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      width: 310,
+      height: 60,
+      decoration: BoxDecoration(gradient: LinearGradient(
+          begin: Alignment.centerLeft, end: Alignment.centerRight,
+          colors: [Color(0xFFE86B02), Color(0xFFFA9541)]),
+          borderRadius: BorderRadius.circular(25)),
+      child: ElevatedButton(onPressed: (){}, child: Text(
+        text,
+        style: TextStyle(fontFamily: 'Nunito',
+        fontSize: 24,
+          fontWeight: FontWeight.w700
+        ),
+      ), style: ElevatedButton.styleFrom(backgroundColor: Colors.transparent, elevation: 0, shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25))),)
+    );
+  }
+}
--- wordfind_app/lib/start_page.dart
+++ wordfind_app/lib/start_page.dart
@@ -1,4 +1,6 @@
+import 'package:day_11_flutter/Start_Button.dart';
 import 'package:day_11_flutter/gradient_text.dart';
+import 'package:day_11_flutter/input_field.dart';
 import 'package:flutter/material.dart';
 
 class StartPage extends StatelessWidget {
@@ -40,42 +42,48 @@ class StartPage extends StatelessWidget {
                const Padding(padding: EdgeInsets.only(top: 20)),
               const GradientText('Player Name', 20.0),
               const Padding(padding: EdgeInsets.only(top: 20)),
+              const Padding(padding: EdgeInsets.only(top: 20)),
+                InputField()
             ],
           ),
         ),
       ),
-      floatingActionButton: const StartButton('Start'),
+      floatingActionButton: StartButton('START'),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
-class StartButton extends StatelessWidget {
-  const StartButton(String s, {super.key});
+// class StartButton extends StatelessWidget {
+//   const StartButton(String s, {super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       width: 310,
+//       height: 60,
+//       decoration: BoxDecoration(
+//         gradient: const LinearGradient(begin: Alignment.centerLeft,
+//           end: Alignment.centerRight,
+//           colors: [Color(0xFFE86B02), Color(0xFFFA9541)],
+//         ),
+//         borderRadius: BorderRadius.circular(25),
+//       ),
+//       child: StartButton('START'),
+//       // child: ElevatedButton(onPressed: () {},
+//       //     child: ElevatedButton(style: ElevatedButton.styleFrom(
+//       //         backgroundColor: Colors.transparent,
+//       //         elevation: 0,
+//       //         shape: RoundedRectangleBorder(
+//       //             borderRadius: BorderRadius.circular(25))),
+//       //       onPressed: () {  },
+//       //       child: const Text('START', style: TextStyle(fontFamily: 'Nunito',
+//       //           fontSize: 24,
+//       //           fontWeight: FontWeight.w700),),)
+//       // ),
+//     );
+//   }
+// }
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 310,
-      height: 60,
-      decoration: BoxDecoration(
-        gradient: const LinearGradient(begin: Alignment.centerLeft,
-          end: Alignment.centerRight,
-          colors: [Color(0xFFE86B02), Color(0xFFFA9541)],
-        ),
-        borderRadius: BorderRadius.circular(25),
-      ),
-      child: ElevatedButton(onPressed: () {},
-          child: ElevatedButton(style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.transparent,
-              elevation: 0,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25))),
-            onPressed: () {  },
-            child: const Text('START', style: TextStyle(fontFamily: 'Nunito',
-                fontSize: 24,
-                fontWeight: FontWeight.w700),),)
-      ),
-    );
-  }
-}
+
 
 
